dcfbef51b24d6d945c551da19555ba07a03c1b42
Matthew Paoletta <156368976+mpaoletta73@users.noreply.github.com>
2026-04-16 21:56:18 -0700

    Lab 3 first draft

---
+++ global.js
@@ -0,0 +1,88 @@
+console.log("IT'S ALIVE!");
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+const BASE_PATH =
+  location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+    ? '/'
+    : '/portfolio/';
+
+const pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'cv.html', title: 'Resume' },
+  { url: 'https://github.com/mpaoletta73', title: 'GitHub' },
+];
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+    <label class="color-scheme">
+      Theme:
+      <select>
+        <option value="light dark">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>`,
+);
+
+const nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  const a = document.createElement('a');
+  a.href = url;
+  a.textContent = p.title;
+
+  a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname,
+  );
+
+  if (a.host !== location.host) {
+    a.target = '_blank';
+    a.rel = 'noopener noreferrer';
+  }
+
+  nav.append(a);
+}
+
+const select = document.querySelector('.color-scheme select');
+
+function setColorScheme(colorScheme) {
+  document.documentElement.style.setProperty('color-scheme', colorScheme);
+  select.value = colorScheme;
+}
+
+if ('colorScheme' in localStorage) {
+  setColorScheme(localStorage.colorScheme);
+}
+
+select.addEventListener('input', function (event) {
+  const colorScheme = event.target.value;
+  setColorScheme(colorScheme);
+  localStorage.colorScheme = colorScheme;
+});
+
+const form = document.querySelector('form');
+
+form?.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const data = new FormData(form);
+  const params = [];
+
+  for (let [name, value] of data) {
+    params.push(`${name}=${encodeURIComponent(value)}`);
+  }
+
+  const url = `${form.action}?${params.join('&')}`;
+  location.href = url;
+});
